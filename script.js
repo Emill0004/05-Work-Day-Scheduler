7cd96eb0be0dc56765a84bbb9c4bb89e6f94f897
Emill0004 <e.s.miller13125@gmail.com>
2023-03-23 17:42:05 -0600

    added new hour divs for each work hour.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // in the html.
 $(function () {
   // var currentDay = document.getElementById("currentday");
-  $('#currentDay').text(dayjs().format('MMM D, YYYY'));
+  $('#currentDay').text(dayjs().format('dddd, MMMM D'));
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
