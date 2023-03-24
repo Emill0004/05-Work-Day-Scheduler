38ab0c929e05bfece8cdbc0949b1f481a532a53b
Emill0004 <e.s.miller13125@gmail.com>
2023-03-23 18:09:27 -0600

    added a for loop that changes the classes of each hour div if its before 9am or after 6pm.

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+var hr9El = document.getElementById("hour-9");
+var hr10El = document.getElementById("hour-10");
+var hr11El = document.getElementById("hour-11");
+var hr12El = document.getElementById("hour-12");
+var hr1El = document.getElementById("hour-1");
+var hr2El = document.getElementById("hour-2");
+var hr3El = document.getElementById("hour-3");
+var hr4El = document.getElementById("hour-4");
+var hr5El = document.getElementById("hour-5");
+var hrArray = [hr9El, hr10El, hr11El, hr12El, hr1El, hr2El, hr3El, hr4El, hr5El];
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
   // var currentDay = document.getElementById("currentday");
   $('#currentDay').text(dayjs().format('dddd, MMMM D'));
+  console.log(dayjs().hour());
+  for (var i = 0; i < hrArray.length; i++) {
+    if (dayjs().hour() >= 18) {
+      hrArray[i].classList.toggle("past");
+    } else if (dayjs().hour() <= 8) {
+      hrArray[i].classList.toggle("future");
+    }
+  }
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
